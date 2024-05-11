076aa6e6baca8b3263a1823c515f6ae72518aff5
tiepnghienvc22 <nanlovecappun0suy@hotmail.com>
2024-05-11 19:22:35 +0700

    test

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-const heapSort = (arr) => {
-  const heapify = (arr, n, i) => {
-    let largest = i;
-    const left = 2 * i + 1;
-    const right = 2 * i + 2;
-    if (left < n && arr[left] > arr[largest]) {
-      largest = left;
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
     }
-    if (right < n && arr[right] > arr[largest]) {
-      largest = right;
-    }
-    if (largest !== i) {
-      [arr[i], arr[largest]] = [arr[largest], arr[i]];
-      heapify(arr, n, largest);
-    }
-  };
-  const buildHeap = (arr) => {
-    const n = arr.length;
-    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
-      heapify(arr, n, i);
-    }
-  };
-  buildHeap(arr);
-  for (let i = arr.length - 1; i > 0; i--) {
-    [arr[0], arr[i]] = [arr[i], arr[0]];
-    heapify(arr, i, 0);
   }
-  return arr;
-};
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
